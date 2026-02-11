4c41f0d981f049fe87c64aa43108554187c594d9
Dasioreq <adammarzec112@gmail.com>
2026-02-11 12:53:44 +0100

    feat: add collision detection when generating level

--- Assets/LevelRooms/GeneratorScript.cs
+++ Assets/LevelRooms/GeneratorScript.cs
@@ -1,40 +1,82 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class GeneratorBehavour : MonoBehaviour
+public class GeneratorBehaviour : MonoBehaviour
 {
     [SerializeField] private List<Room> rooms;
     [SerializeField] private Room origin;
+    [SerializeField] private LayerMask roomMask;
 
     void generate(uint roomNumber)
     {
         Vector3 position = Vector3.zero;
         Vector3 direction = origin.exitDirection;
 
-        origin.CreateInstance(position, direction);
-
         position += origin.exit;
-        direction = origin.exitDirection;
 
-        for(uint _ = 0; _ < roomNumber; _++)
+        List<int> indices = new List<int>();
+        for(int i = 0; i < rooms.Count; i++)
         {
-            int i = UnityEngine.Random.Range(0, rooms.Count);
+            for(int j = 0; j < rooms[i].weight; j++)
+            {
+                indices.Add(i);
+            }
+        }
 
+        List<(GameObject instance, Vector3 position, Vector3 direction, int index, List<int> possibleIndices)> generatedRooms = new List<(GameObject, Vector3, Vector3, int, List<int>)>{(origin.CreateInstance(position, direction), Vector3.zero, origin.exitDirection, -1, new List<int>(indices))};
+
+        for(int roomIndex = 1; roomIndex <= roomNumber; roomIndex++)
+        {
+            var lastRoom = generatedRooms[roomIndex - 1];
+
+            var availibleIndices = lastRoom.possibleIndices;
+
+            if(availibleIndices.Count == 0)
+            {
+                if(roomIndex >= 1)
+                {
+                    Destroy(lastRoom.instance);
+                    position = lastRoom.position;
+                    direction = lastRoom.direction;
+                    generatedRooms[roomIndex - 2].possibleIndices.RemoveAll(el => el == lastRoom.index);
+                    generatedRooms.RemoveAt(roomIndex - 1);
+                    roomIndex -= 2;
+                    continue;
+                }
+            }
+
+            int i = availibleIndices[UnityEngine.Random.Range(0, availibleIndices.Count)];
+            
             Room r = rooms[i];
-            r.CreateInstance(position, direction);
 
-            position += Quaternion.AngleAxis(Vector3.Angle(direction, Vector3.forward), Vector3.up) * r.exit;
+            GameObject instance = r.CreateInstance(position, direction);
 
-            float angle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
-            direction = Quaternion.AngleAxis(angle, Vector3.up) * r.exitDirection;
+            if(Room.isColliding(instance, generatedRooms.Select(room => room.instance).ToArray<GameObject>()[..^1]))
+            {
+                Destroy(instance);
+
+                availibleIndices.RemoveAll(el => el == i);
+                roomIndex--;
+            }
+            else
+            {
+                generatedRooms.Add((instance, position, direction, i, new List<int>(indices)));
+
+                float angle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
+                Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+                position += rotation * r.exit;
+                direction = rotation * r.exitDirection;
+            }
         }
     }
 
     void Awake()
     {
-        generate(8);
+        generate(100);
     }
 }
--- Assets/LevelRooms/Room.cs
+++ Assets/LevelRooms/Room.cs
@@ -6,9 +6,52 @@ public class Room : ScriptableObject
     [SerializeField] public Vector3 exit;
     [SerializeField] public Vector3 exitDirection;
     [SerializeField] public GameObject prefab;
+    [SerializeField] public uint weight = 1;
 
     public GameObject CreateInstance(Vector3 position, Vector3 direction)
     {
         return Instantiate(prefab, position, Quaternion.LookRotation(direction));
+    }
+
+    public static bool isColliding(GameObject instantiated, GameObject[] existingRooms)
+    {
+        var boundingBox = instantiated.transform.Find("Bounds").GetComponent<Collider>().bounds;
+
+        // Vector3 center = boundingBox.center;
+        // Vector3 extents = boundingBox.extents - Vector3.one * 0.05f;
+
+        // Quaternion rotation = Quaternion.LookRotation(direction);
+
+        // // float angle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
+        // // center = Quaternion.AngleAxis(angle, Vector3.up) * center;
+        // // extents = Quaternion.AngleAxis(angle, Vector3.up) * extents;
+
+        // // center += position;
+        // // Vector3 newDirection = Quaternion.AngleAxis(angle, Vector3.up) * exitDirection;
+        // // center += (newDirection + direction).normalized;
+
+        // Debug.DrawLine(center - extents, center + extents, Color.red, 99999);
+
+        // Physics.SyncTransforms();
+
+        // Collider[] collisions = Physics.OverlapBox(
+        //     center,
+        //     extents,
+        //     Quaternion.identity,
+        //     layer
+        // );
+
+        foreach(GameObject room in existingRooms)
+        {
+            Collider collision = room.transform.Find("Bounds").GetComponent<Collider>();
+
+            if((instantiated.transform.position - room.transform.position).magnitude > boundingBox.size.magnitude + collision.bounds.size.magnitude)
+                continue;
+            
+            if(boundingBox.Intersects(collision.bounds))
+                return true;
+        }
+
+        return false;
     }
 }
