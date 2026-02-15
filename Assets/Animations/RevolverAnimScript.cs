bf8410e9ff10065d339b7d98e707a83054f57695
arma-j <jarmatys94@gmail.com>
2026-02-15 21:02:30 +0100

    feat: add basic firearm mechanics and sfx

--- Assets/Animations/RevolverAnimScript.cs
+++ Assets/Animations/RevolverAnimScript.cs
@@ -1,28 +1,69 @@
 using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class RevolverAnimScript : MonoBehaviour
 {
     private Animator anim;
+    private bool isOn = false;
+    [SerializeField] private float fireOffset;
+    [SerializeField] private float reloadOffset1;
+    [SerializeField] private float reloadOffset2;
+    [SerializeField] private AudioClip shotSound;
+    [SerializeField] private AudioClip reloadSound;
+    [SerializeField] private AudioClip alternatifeReloadSound;
+    private AudioSource source;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if(anim != null)
+        if(anim != null && !isOn)
         {
             if(Input.GetMouseButtonDown(0))
             {
-                anim.SetTrigger("TrFire");
+                StartCoroutine(Fire());
             }
 
             if(Input.GetKeyDown("r"))
             {
-                anim.SetTrigger("TrReload");
+                StartCoroutine(Reload());
             }
         }
+    }
+    IEnumerator Fire()
+    {
+        isOn = true;
+        source.PlayOneShot(shotSound);
+        anim.SetTrigger("TrFire");
+
+        yield return new WaitForSeconds(fireOffset);
+
+        isOn = false;
+    }
+    IEnumerator Reload()
+    {
+        isOn = true;
+        anim.SetTrigger("TrReload");
+
+        yield return new WaitForSeconds(reloadOffset1);
+        int chance = Random.Range(0, 100);
+        if (chance < 20)
+        {
+            source.PlayOneShot(alternatifeReloadSound);
+        }
+        else
+        {
+            source.PlayOneShot(reloadSound);
+        }
+
+        yield return new WaitForSeconds(reloadOffset2);
+
+        isOn = false;
     }
 }
---
+++ Assets/Scripts/gun.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+    [SerializeField] private float gunDamage;
+    [SerializeField] private Camera cam;
+    [SerializeField] private float fireCooldown;
+
+    private float cooldown = .0f;
+
+    private void Update()
+    {
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Vector3 origin = cam.transform.position;
+                Vector3 direction = cam.transform.forward;
+
+                if (Physics.Raycast(origin, direction, out hit))
+                {
+                    Debug.Log(hit.collider.gameObject.name + origin);
+                }
+
+                cooldown = fireCooldown;
+            }
+        }
+    }
+}
