1e4cfa10342a5055c7392b4a334da9687df9a213
Dasioreq <adammarzec112@gmail.com>
2026-02-18 18:10:52 +0100

    feat: adjust card panel logic to block weapon usage

--- Assets/Scripts/Cards/Cards.cs
+++ Assets/Scripts/Cards/Cards.cs
@@ -7,14 +7,19 @@ public class Cards : MonoBehaviour
     public GameObject CardPanel;
     public GameObject Player;
     public GameObject Camera;
+    public GameObject weapon;
+
     private void ShowCards()
     {
         Camera.GetComponent<CameraContoller>().enabled = false;
+        Player.GetComponent<MovementHandler>().rb.linearVelocity = Vector3.zero;
         Player.GetComponent<MovementHandler>().enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         CardPanel.SetActive(true);
-
+        weapon.GetComponent<Animator>().enabled = false;
+        weapon.GetComponent<AudioSource>().enabled = false;
+        weapon.GetComponent<gun>().enabled = false;
     }
 
     public void CloseWin()
@@ -24,6 +29,9 @@ public class Cards : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = false;
         CardPanel.SetActive(false);
+        weapon.GetComponent<Animator>().enabled = true;
+        weapon.GetComponent<AudioSource>().enabled = true;
+        weapon.GetComponent<gun>().enabled = true;
     }
 
     void Start()
