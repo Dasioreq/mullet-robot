d9e6ed733604db02e3a856f863631d241caafc17
SilentWolves-Imagine <imagine.silentwolves@interia.pl>
2026-03-27 21:13:31 +0100

    feat: rare bonuses setup

--- Assets/Scripts/Cards/RandCards.cs
+++ Assets/Scripts/Cards/RandCards.cs
@@ -16,13 +16,15 @@ public class RandCards : MonoBehaviour
     public PlayerDamage playerDmg;
 
     public EquipWeapon curWeap;
-    private List<int> weaponBases = new List<int> {0,3};
-    private int[] weaponProgress = new int[] {0,0};
-    private string[] weaponNames = {"A","B"};
+    private List<int> weaponBases = new List<int> {0,3,6};
+    private int[] weaponProgress = new int[] {0,0,0};
+    private string[] weaponNames = {"A","B","C"};
 
-    public enum UpgradeType{speed, jump, dashing, health, dashingTime, newWeapon, upgradeWeapon}
+    public enum UpgradeType{speed, jump, dashing, health, dashingTime, newWeapon, upgradeWeapon, extraA, extraB, extraC}
     private Dictionary<UpgradeType, float> currentMultipliers = new Dictionary<UpgradeType, float>();
     private float startMultiplier = 1.10f;
+    private List<UpgradeType> rareUpgrades = new List<UpgradeType>(){UpgradeType.extraA, UpgradeType.extraB, UpgradeType.extraC};
+    private List<UpgradeType> usedRareUpgrades = new List<UpgradeType>();
 
     private void Awake()
     {
@@ -44,9 +46,8 @@ public class RandCards : MonoBehaviour
     public void RCards(GameObject[] spawnedButtons)
     {
         var available = Enum.GetValues(typeof(UpgradeType))
-        .Cast<UpgradeType>()
-        .Where(t => currentMultipliers.ContainsKey(t) && currentMultipliers[t] < 1.5f)
-        .ToList();
+        .Cast<UpgradeType>().Where(t => currentMultipliers.ContainsKey(t) && currentMultipliers[t] < 1.5f && !usedRareUpgrades.Contains(t)).ToList();
+
         if (available.Count == 0)
         {
             GetComponent<Cards>().CloseWin();
@@ -79,9 +80,32 @@ public class RandCards : MonoBehaviour
             Bonuses bonusScript = btnObj.GetComponent<Bonuses>();
             if (bonusScript != null)
             {
-                int randomIndex = UnityEngine.Random.Range(0, available.Count);
-                UpgradeType randomType = available[randomIndex];
-                available.RemoveAt(randomIndex);
+
+                UpgradeType randomType;
+
+                float randomIndex = UnityEngine.Random.Range(0f, 100f);
+                bool found = false;
+                randomType = available[0];
+
+                var currentRares = available.Where(t => rareUpgrades.Contains(t)).ToList();
+                var currentCommons = available.Where(t => !rareUpgrades.Contains(t)).ToList();
+
+                for (int i = 0; i < currentRares.Count; i++)
+                {
+                    if (randomIndex <= (i + 1) * 3f && randomIndex > i * 3f)
+                    {
+                        randomType = currentRares[i];
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found)
+                {
+                    var pool = currentCommons.Count > 0 ? currentCommons : available;
+                    randomType = pool[UnityEngine.Random.Range(0, pool.Count)];
+                }
+
+                available.Remove(randomType);
 
                 UpgradeData upgrade = new UpgradeData
                 {
@@ -118,33 +142,48 @@ public class RandCards : MonoBehaviour
             multiplierUpgrade = (currentMultipliers[upgr.type] + 0.05f > 1.2f) ? 0.02f : 0.05f;
             currentMultipliers[upgr.type] += multiplierUpgrade;
         }
-            switch (upgr.type)
-            {
-                case UpgradeType.speed:
-                    player.acceleration *= (1 + multiplierUpgrade);
-                    player.maxVelocity *= (1 + multiplierUpgrade);
-                    break;
-                case UpgradeType.jump:
-                    player.jumpHeight *= (1 + multiplierUpgrade);
-                    break;
-                case UpgradeType.dashing:
-                    player.dashForce *= (1 + multiplierUpgrade);
-                    break;
-                case UpgradeType.health:
-                    playerDmg.maxLifeTime *= (1 + multiplierUpgrade);
-                    break;
-                case UpgradeType.dashingTime:
-                    player.dashCooldownTime *= (1 + multiplierUpgrade);
-                    break;
-                case UpgradeType.newWeapon:
-                    int family = upgr.weaponID / 3;
-                    weaponProgress[family] = upgr.weaponID % 3;
-                    curWeap.Equip(upgr.weaponID);
-                    break;
-                default:
-                    Debug.Log("Different option");
-                    break;
-            }
+
+        switch (upgr.type)
+        {
+            case UpgradeType.speed:
+                player.acceleration *= (1 + multiplierUpgrade);
+                player.maxVelocity *= (1 + multiplierUpgrade);
+                break;
+            case UpgradeType.jump:
+                player.jumpHeight *= (1 + multiplierUpgrade);
+                break;
+            case UpgradeType.dashing:
+                player.dashForce *= (1 + multiplierUpgrade);
+                break;
+            case UpgradeType.health:
+                playerDmg.maxLifeTime *= (1 + multiplierUpgrade);
+                break;
+            case UpgradeType.dashingTime:
+                player.dashCooldownTime *= (1 + multiplierUpgrade);
+                break;
+            case UpgradeType.newWeapon:
+                int family = upgr.weaponID / 3;
+                weaponProgress[family] = upgr.weaponID % 3;
+                curWeap.Equip(upgr.weaponID);
+                break;
+            case UpgradeType.extraA:
+                Debug.Log("Extra A");
+                break;
+            case UpgradeType.extraB:
+                Debug.Log("Extra B");
+                break;
+            case UpgradeType.extraC:
+                Debug.Log("Extra C");
+                break;
+            default:
+                Debug.Log("Different option");
+                break;
+        }
+
+        if (rareUpgrades.Contains(upgr.type))
+        {
+            usedRareUpgrades.Add(upgr.type);
+        }
 
         GetComponent<Cards>().CloseWin();
         foreach (var b in Btn) 
