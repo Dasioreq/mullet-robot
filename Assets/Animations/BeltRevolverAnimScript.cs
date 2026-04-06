35b3ef2b7cf1256888129b5b2f6fb4d5868cd4c1
arma-j <jarmatys94@gmail.com>
2026-04-07 01:30:07 +0200

    feat: add belt revolver particles and correct grenade launcher casings

--- Assets/Animations/BeltRevolverAnimScript.cs
+++ Assets/Animations/BeltRevolverAnimScript.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class BeltRevolverAnimScript : Actions
 {
+    [SerializeField] private ParticleSystem[] chargeObjects;
+    [SerializeField] float smokeTimey;
     override public IEnumerator Fire()
     {
         anim.SetTrigger("TrFire");
         StartCoroutine(Flash(.1f));
         foreach(var animator in GetComponentsInChildren<Animator>())
         {
+            chargeObjects[0].Play();
+            chargeObjects[1].Play();
+            chargeObjects[2].Play();
+            chargeObjects[3].Play();
+            int chance2 = Random.Range(0, 2);
+            if (chance2 < 1)
+            {
+                chargeObjects[4].Play();
+                chargeObjects[5].Play();
+            }           
             animator.SetTrigger("TrFire");
+            
         }
         foreach(var casing in GetComponentsInChildren<AddCasing>())
         {
@@ -19,6 +32,9 @@ public class BeltRevolverAnimScript : Actions
         }
         yield return new WaitForSeconds(fireSfxDelay);
         source.PlayOneShot(shotSound);
+        yield return new WaitForSeconds(smokeTimey);
+        chargeObjects[4].Stop();
+        chargeObjects[5].Stop();
         yield break;
     }
 }
