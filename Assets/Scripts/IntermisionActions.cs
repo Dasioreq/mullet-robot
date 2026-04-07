f479037bc4e81d17a11c752bdc308644eb74bf4c
Dasioreq <adammarzec112@gmail.com>
2026-04-07 18:59:34 +0200

    chore: merge with level-gen

--- Assets/Scripts/IntermisionActions.cs
+++ Assets/Scripts/IntermisionActions.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using static GameController;
 
@@ -17,30 +18,57 @@ public class IntermisionActions : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "MainCamera")
-            StartCoroutine(jukebox.Switch(false));
+        if(!endLevel)
+            if(other.gameObject.tag == "MainCamera")
+                StartCoroutine(jukebox.Switch(false));
 
         if(other.gameObject.tag == "MainCamera")
             if(endLevel)
             {
-                gameController.GenerateLevel();
-
-                var player = GameObject.FindWithTag("Player");
-
-                if(player)
-                {
-                    player.transform.position = Quaternion.Inverse(transform.rotation) * (player.transform.position - transform.position) + new Vector3(0, 0, -7.5f);
-                    gameController.level++;
-                    player.GetComponent<Rigidbody>().linearVelocity = Quaternion.Inverse(transform.rotation) * player.GetComponent<Rigidbody>().linearVelocity;
-                }
-
-                other.gameObject.GetComponent<CameraContoller>().SetRotation(Quaternion.Inverse(transform.rotation) * other.transform.rotation);
+                StartCoroutine(RegenerateLevel(other, 1f));
             }
+    }
+
+    public void ManualMusicSwitch()
+    {
+        StartCoroutine(jukebox.Switch(false));
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.tag == "MainCamera")
-            StartCoroutine(jukebox.Switch(true));
+        if(!endLevel)
+            if(other.gameObject.tag == "MainCamera")
+                StartCoroutine(jukebox.Switch(true));
+    }
+
+    IEnumerator RegenerateLevel(Collider other, float time)
+    {
+        foreach(var anim in GetComponentsInChildren<Animator>())
+        {
+            anim.SetTrigger("Tr");
+        }
+
+        float elapsed = 0;
+
+        while(elapsed < time)
+        {
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        gameController.GenerateLevel();
+
+        var player = GameObject.FindWithTag("Player");
+
+        if(player)
+        {
+            player.transform.position = Quaternion.Inverse(transform.rotation) * (player.transform.position - transform.position) + new Vector3(0, 0, -7.5f);
+            gameController.level++;
+            player.GetComponent<Rigidbody>().linearVelocity = Quaternion.Inverse(transform.rotation) * player.GetComponent<Rigidbody>().linearVelocity;
+            player.GetComponent<Rigidbody>().MovePosition(Quaternion.Inverse(transform.rotation) * (player.transform.position - transform.position) + new Vector3(0, 0, -7.5f));
+        }
+
+        other.gameObject.GetComponent<CameraContoller>().SetRotation(Quaternion.Inverse(transform.rotation) * other.transform.rotation);
+        yield break;
     }
 }
